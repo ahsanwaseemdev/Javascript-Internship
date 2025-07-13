1b2e921296534032fbe899219c64689435ec6566
Ahsan Waseem <digitalcraftc@gmail.com>
2025-07-13 15:03:42 +0500

    Day 7 Material

---
+++ challenge1p3.js
@@ -0,0 +1,33 @@
+/*
+........................Coding Challenge #1...............................
+
+//........Data 1............
+let arr = [17, 21, 23];
+
+function printForecast(arr) {
+  forecast = "";
+  for (let i = 0; i < arr.length; i++) {
+    forecast = forecast += `${arr[i]}ºC in day ${i + 1} ... `;
+  }
+  console.log("...." + forecast);
+}
+
+printForecast(arr);
+
+
+
+
+//........Data 2............
+let arr = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  forecast = "";
+  for (let i = 0; i < arr.length; i++) {
+    forecast = forecast += `${arr[i]}ºC in day ${i + 1} ... `;
+  }
+  console.log("...." + forecast);
+}
+
+printForecast(arr);
+
+*/
