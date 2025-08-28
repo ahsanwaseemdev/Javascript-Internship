11bfd9e436d0034abb28337497ea64c6b4b2b482
Ahsan Waseem <digitalcraftc@gmail.com>
2025-08-28 01:00:32 -0700

    Add files via upload

---
+++ day18.js
@@ -0,0 +1,36 @@
+"Use Strict";
+/*
+//dEposite
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+//Longer Method
+const depositFor = [];
+for (const mov of movements) {
+  if (mov > 0) {
+    depositFor.push(mov);
+  }
+}
+console.log(depositFor);
+//widthrawals
+const widthrawalsFor = movements.filter(function (wdMov) {
+  return wdMov < 0;
+});
+console.log(widthrawalsFor);
+
+//////////////
+const euroToUsd = 1.1;
+const movementsUSD = movements.map(function (mov) {
+  return mov * euroToUsd;
+});
+//console.log(movements);
+console.log(movementsUSD);
+
+// //With Arrow function
+// const movementsarrow = movements.map((mov) => mov * euroToUsd);
+// console.log(movementsarrow);
+
+
+*/
---
+++ script.js
@@ -0,0 +1,17 @@
+"Use Strict";
+/*
+const CheckDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const Dogs = dogsJuliaCorrected.concat(dogsKate);
+  Dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult🦮, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is an Puppy🐶, and is ${dog} years old`);
+    }
+  });
+};
+CheckDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+*/
